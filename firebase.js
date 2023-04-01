d455b5db9161a47f39ea7cc3d723b9f7d24c10ce
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-04-01 17:35:56 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-app.js";
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKMNqN_HboWQ1D7gKgFlh2CsuoRVIb-sc",
+    authDomain: "aakrati-be79d.firebaseapp.com",
+    databaseURL: "https://aakrati-be79d-default-rtdb.firebaseio.com",
+    projectId: "aakrati-be79d",
+    storageBucket: "aakrati-be79d.appspot.com",
+    messagingSenderId: "210551691064",
+    appId: "1:210551691064:web:1c298db5c025d722d893cd"
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
